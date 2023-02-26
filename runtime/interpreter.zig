9a5c21e7006b80afe9a370db126088f349635493
luizpbraga <portilho.braga@gmail.com>
2023-02-26 11:41:31 -0300

    update: evaluate suports Assignment Expressions

--- runtime/interpreter.zig
+++ runtime/interpreter.zig
@@ -3,6 +3,7 @@ const env = @import("environment.zig");
 const val = @import("values.zig");
 const exp = @import("eval/expression.zig");
 const stmt = @import("eval/statemant.zig");
+const std = @import("std");
 
 pub fn evaluate(astStmt: ast.Statement, scope: *env.Environment) anyerror!val.RuntimeValue {
     return switch (astStmt.kind()) {
@@ -10,14 +11,15 @@ pub fn evaluate(astStmt: ast.Statement, scope: *env.Environment) anyerror!val.Ru
         .NumericLiteral => .{ .numberValue = .{ .value = astStmt.expression.numericLiteral.value } },
         // .NullLiteral => .{ .nullValue = .{} },
         .BinaryExpr => try exp.evalBinExpr(astStmt.expression.binaryExpr, scope),
+        .AssignmentExpr => try exp.evalAssignment(astStmt.expression.assignmentExpr, scope),
         // Statement
         .Program => try stmt.evalProgram(astStmt.program, scope),
         .VarDeclaration => try stmt.evalVarDeclaration(astStmt.varDeclaration, scope),
+        // else => {
+        //     std.debug.print("EVALUATION ERROR: astStmt {}\n", .{astStmt});
+        //     return error.TokeNotDefined;
+        // },
     };
-    // else => {
-    //         std.debug.print("astStmt {}", .{astStmt});
-    //         return error.TokeNotDefined;
-    //     },;
 }
 // fn evalBinExpr(bexp: ast.BinaryExpr, scope: env.Environment) anyerror!val.RuntimeValue {
 //     const left_val = try evaluate(.{ .expression = bexp.left.?.* }, scope);
